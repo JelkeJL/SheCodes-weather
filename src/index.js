18d078320ff2f810f1da57d1398cd139fd82d430
Jelke Joosen <jelkejoosen@hotmail.com>
2021-10-21 11:57:15 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -106,6 +106,26 @@ function showForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+
+  google.charts.load("current", { packages: ["corechart"] });
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ["Year", "Sales", "Expenses"],
+      ["2004", 1000, 400],
+      ["2005", 1170, 460],
+      ["2006", 660, 1120],
+      ["2007", 1030, 540],
+    ]);
+
+    var chart = new google.visualization.LineChart(
+      document.getElementById("curve_chart")
+    );
+
+    chart.draw(data);
+  }
+  drawChart();
 }
 
 //get coordinates
